f4cf293c5283bd27e130f5b0fd29614cc292bc84
Pawel Cendrzak <pabloxxl@fastmail.fm>
2015-08-22 17:55:48 +0200

    Moved logic to object.
    Moved everything to object.parse
    Added main section.
    Added error codes, and returning them.

--- git-shortstatus.py
+++ git-shortstatus.py
@@ -5,38 +5,54 @@ import subprocess
 import sys
 
 VERSION = "0.1"
-# Parse program arguments
-parser = argparse.ArgumentParser(description="git-info v"+VERSION,
-                                 epilog="developed by Pawel Cendrzak")
 
-parser.add_argument('DIR', help='Git root directory',
-                    nargs='?', default='~')
 
-parser.add_argument("-d", "--debug",
-                    help="enter debug mode",
-                    action="store_true")
+class code:
+    OK = 0
+    REV_PARSE_ERROR = 1
+    GIT_STATUS_ERROR = 2
+    UNSUPORTED_PARAM = 3
 
-args = parser.parse_args()
-pRoot = subprocess.Popen(['git', 'rev-parse', '--show-toplevel'],
-                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-out, err = pRoot.communicate()
-if err:
-    print err.rstrip()
-    sys.exit()
+class shortStatus(object):
 
-root = out.rstrip()
+    def parse(self):
+        # Parse program arguments
+        parser = argparse.ArgumentParser(description="git-info v"+VERSION,
+                                         epilog="developed by Pawel Cendrzak")
 
-pStatus = subprocess.Popen(['git', 'status', root, '--porcelain'],
-                           stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-out, err = pStatus.communicate()
-if err:
-    print err.rstrip()
-    sys.exit()
+        parser.add_argument("--sum", "-s", "--debug",
+                            help="enter debug mode",
+                            action="store_true")
 
-status = out.rstrip()
+        args = parser.parse_args()
+        if args.sum:
+            return code.UNSUPORTED_PARAM
+        pRoot = subprocess.Popen(['git', 'rev-parse', '--show-toplevel'],
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.PIPE)
 
-if status:
-    print status
-else:
-    print "Clean!"
+        out, err = pRoot.communicate()
+        if err:
+            return code.REV_PARSE_ERROR
+
+        root = out.rstrip()
+
+        pStatus = subprocess.Popen(['git', 'status', root, '--porcelain'],
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+
+        out, err = pStatus.communicate()
+        if err:
+            return code.GIT_STATUS_ERROR
+
+        self.statusList = out.rstrip().split("\n")
+        return code.OK
+
+if __name__ == "__main__":
+    s = shortStatus()
+    returnCode = s.parse()
+    if returnCode:
+        print "Git shortstatus returned code " + str(returnCode)
+        sys.exit(returnCode)
+    sys.exit(0)
